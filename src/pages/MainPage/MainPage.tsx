a09ea39b64df995f408d67aff5a6c33f9f56921f
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-07-23 16:35:35 +0300

    feat: Очистила проект, написала пример добавления компонента

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export const MainPage = () => {
+  return (
+    <div className={styles.mainContainer}>
+      {/*Тут будут лежать компоненты из папки components*/}
+      {/*собирать главную страницу будем здесь*/}
+      Main Page
+    </div>
+  );
+};

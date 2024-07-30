361dd1fbcd0a8fa14b614202b8c6d649747c12bf
sitl_idnm <mironovlev3@gmail.com>
2024-07-30 11:59:01 +0300

    feat: add Introduce and Changer components

---
+++ src/components/Introduce/Introduce.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+export const Introduce = () => {
+	return (
+		<>
+			<section className={styles.wrapper}>
+				<h1 className={styles.title}>Freshly Churned Every Day.</h1>
+			</section>
+		</>
+	)
+}
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.css';
 import {Info} from '../../components/Info'
+import {Changer} from '../../components/Changer'
+import {Introduce} from '../../components/Introduce'
 
 export const MainPage = () => {
   return (
@@ -7,6 +9,8 @@ export const MainPage = () => {
       {/*Тут будут лежать компоненты из папки components*/}
       {/*собирать главную страницу будем здесь*/}
       Main Page
+      <Introduce />
+      <Changer />
       <Info/>
     </div>
   );

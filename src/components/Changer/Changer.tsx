605da7a6d76df5af615adf5ee48bb1cbe8ce35b9
sitl_idnm <mironovlev3@gmail.com>
2024-07-30 11:04:39 +0300

    feat: add adaptive for title from Introduce

--- src/components/Changer/Changer.tsx
+++ src/components/Changer/Changer.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import { useState} from 'react';
 import styles from './styles.module.css'
 
 const iceCreamImages = [
@@ -36,7 +36,7 @@ export const Changer = () => {
             className={`image ${index === currentImageIndex ? 'active' : ''}`}
           />
         ))}
-      </div>
+        </div>
 				<button onClick={handleNext} className={styles.button}></button>
 			</section>
 		</>

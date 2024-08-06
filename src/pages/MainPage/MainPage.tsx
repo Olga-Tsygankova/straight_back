9945abad4b8da005395d6bf1a150e425f96efd37
sitl_idnm <klevtsevitch.lev@yandex.ru>
2024-08-06 12:44:21 +0300

    fix: change Footer.tsx name

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 import {Info} from '../../components/Info';
 import {Introduce} from '../../components/Introduce';
 import { SimpleCarousel } from '../../components/Slider2'
-import { Footer } from '../../components/footer'
+import { Footer } from '../../components/Footer'
 
 
 export const MainPage = () => {

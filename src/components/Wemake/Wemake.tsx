a4c044cb8fc5f8c8f2784c0019992ce722b39d3d
sitl_idnm <klevtsevitch.lev@yandex.ru>
2024-08-07 17:49:12 +0300

    feat: add Wemake component

---
+++ src/components/Wemake/Wemake.tsx
@@ -0,0 +1,55 @@
+import styles from "./styles.module.css";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+export const Wemake: React.FC = () => {
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const lines = textRef.current.children;
+
+      const tl = gsap.timeline({ repeat: -1, yoyo: true });
+      tl.to(lines, { xPercent: -100, duration: 10, ease: "linear" }).to(lines, {
+        xPercent: 0,
+        duration: 10,
+        ease: "linear",
+      });
+
+      gsap.to(lines, {
+        duration: 20,
+        xPercent: -20,
+        ease: "linear",
+        repeat: -1,
+        yoyo: true,
+      });
+    }
+  }, []);
+
+  return (
+    // <section className={styles.wrapper}>
+    // 	<h2 className={styles.title}>We make ice cream the way it used to <br /> be.</h2>
+    // </section>
+    <div className={styles.wrapper}>
+      <div className={styles.inside} ref={textRef}>
+        {Array(30)
+          .fill(null)
+          .map((_, index) => (
+            <div
+              className={`${styles.subtitle} ${styles.noselect}`}
+              key={index}
+            >
+              we make ice cream the way is used to be. we make ice cream the way
+              is used to be. we make ice cream the way is used to be. we make
+              ice cream the way is used to be. we make ice cream the way is used
+              to be. we make ice cream the way is used to be. we make ice cream
+              the way is used to be.
+            </div>
+          ))}
+      </div>
+      <h2 className={styles.title}>
+        we make ice cream the way is used <br /> to be.
+      </h2>
+    </div>
+  );
+};

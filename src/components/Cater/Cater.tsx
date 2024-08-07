b95f69c2fe2a0243a66103ab766bad3cc7247c87
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-07 11:08:52 +0300

    feat: Добавил css и адаптивчик

--- src/components/Cater/Cater.tsx
+++ src/components/Cater/Cater.tsx
@@ -4,16 +4,18 @@ import icecream from '../../assets/Cater/IMAGE.png'
 export const Cater = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.image}>
-				<img src={icecream} alt="Icecream_Photo" />
-			</div>
-			<div className={styles.text}>
-				<h2>
-					Cater Your Next Event With
-				</h2>
-				<p>
-					Too many parties have an un-magical, sad lack of ice cream. The Magic Slab, where ice cream is handmade with love and attention, is changing that. Come treat your guests or team with the best ice cream you've ever tasted.
-				</p>
+			<div className={styles.content}>
+				<div className={styles.image}>
+					<img src={icecream} alt="Icecream_Photo" />
+				</div>
+				<div className={styles.text}>
+					<h2>
+						Cater Your Next Event With
+					</h2>
+					<p>
+						Too many parties have an un-magical, sad lack of ice cream. The Magic Slab, where ice cream is handmade with love and attention, is changing that. Come treat your guests or team with the best ice cream you've ever tasted.
+					</p>
+				</div>
 			</div>
 		</section>
 	)
